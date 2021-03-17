6be1caf517dff18bc0b1665d7a9d347aab728a18
Diego Molina <molinacrespo@gmail.com>
2021-03-17 13:27:24 +0100

    OurWork in page transitions

--- src/components/OurWork.js
+++ src/components/OurWork.js
@@ -5,6 +5,9 @@ import bitcoin from './../img/bitcoin.png';
 import running from './../img/running.png';
 import athleteSmall from './../img/athlete-small.png';
 import { MovieState } from './../movieState';
+import {motion, useAnimation} from 'framer-motion';
+import {Hide} from './../styled/styled.js';
+import { useInView } from "react-intersection-observer";
 
 const WorkStyled = styled.div`
     min-height: 90vh;
@@ -20,12 +23,72 @@ const Movie = styled.div`
     }
 `;
 
-const Line = styled.div`
+const Line = styled(motion.div)`
     padding: 0.2rem;
-    background-color: white;
+    background-image: linear-gradient(to right, #097679 , #00d4ff);
     margin-bottom: 2rem;
     width:100%;
 `;
+
+
+const animateText = {
+    hidden : {opacity: 0},
+    show: { 
+        opacity: 1 ,
+        transition: {duration: 0.3}
+    },
+}
+const animateLine = {
+    hidden : {opacity: 0, x : '-100%' },
+    show: { 
+        opacity: 1 ,
+        x :0, 
+        transition: {duration: 0.3}
+    },
+}
+const animateImg = {
+    hidden : {opacity: 0, scale: 1.5},
+    show: { 
+        opacity: 1 ,
+        scale: 1,
+        transition: { duration: 0.75}
+    },
+}
+
+const Frame1 = styled(motion.div)`
+    position: fixed;    
+    width: 100%;
+    left: 0;
+    height: 100vh;
+    background-color: #ff75a0;
+    z-index: 2;
+`;
+
+const Frame2 = styled(Frame1)`
+    background-color: #fce38a;
+`;
+const Frame3 = styled(Frame1)`
+    background-color: #eaffd0;
+`;
+const Frame4 = styled(Frame1)`
+    background-color: #95e1d3;
+`;
+
+const FrameAnim = {
+    hidden: {x: '-200%', skew: '45deg'},
+    show: {
+        x: '200%', 
+        skew: '0deg',
+        transition: {duration: 2, ease: 'easeOut'}
+    }    
+}
+const FrameContainerAnim = {
+    hidden: {opacity: 1},
+    show: {
+        opacity: 1,
+        transition: {staggerChildren: 0.2, duration: 0.5, ease: 'easeOut'},            
+    }    
+}
 
 const OurWork = () => {
     
@@ -33,18 +96,27 @@ const OurWork = () => {
    
     return ( 
         <WorkStyled>
+            <motion.div variants={FrameContainerAnim}>
+                <Frame1 variants={FrameAnim}></Frame1>
+                <Frame2 variants={FrameAnim}></Frame2>
+                <Frame3 variants={FrameAnim}></Frame3>
+                <Frame4 variants={FrameAnim}></Frame4>
+            </motion.div>
             {
                 movies.map((item, index) => 
                     <Movie key={index}>
-                        <h2>{item.title}</h2>
-                        <Line/>
+                        <motion.h2
+                            variants={animateText}>{item.title}</motion.h2>
+                        <Hide>
+                            <Line variants={animateLine}/>
+                        </Hide>                        
                         <Link to={item.url}>
-                            <img src={item.mainImg} alt={item.title}/>
+                            <motion.img variants={animateImg} src={item.mainImg} alt={item.title}/>
                         </Link>
                     </Movie>
                 )
             }
-        </WorkStyled>
+        </WorkStyled>        
      );
 }
  
