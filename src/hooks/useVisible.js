c4d4e658ea01355bbabe19121870cbedb02db9f2
Diego Molina <molinacrespo@gmail.com>
2021-03-19 12:52:46 +0100

    Hook for make component visible on scroll

--- src/hooks/useVisible.js
+++ src/hooks/useVisible.js
@@ -2,26 +2,25 @@ import React, { useEffect } from 'react';
 import {motion, useAnimation} from 'framer-motion';
 import { useInView } from "react-intersection-observer";
 
-export const FadeInWhenVisible = ({ children }) => {
+export const FadeInWhenVisible = ({ children, variants }) => {
+    
+    const defaultVariant = {show: { opacity: 1, scale: 1 }, hidden: { opacity: 0, scale: 0 }};    
     const controls = useAnimation();
     const [ref, inView] = useInView();
   
     useEffect(() => {
       if (inView) {
-        controls.start("visible");
+        controls.start("show");
       }
     }, [controls, inView]);
-  
+
     return (
       <motion.div
         ref={ref}
         animate={controls}
         initial="hidden"
         transition={{ duration: 0.75 }}
-        variants={{
-          visible: { opacity: 1, scale: 1 },
-          hidden: { opacity: 0, scale: 0 }
-        }}
+        variants={variants ?? defaultVariant}
       >
         {children}
       </motion.div>
